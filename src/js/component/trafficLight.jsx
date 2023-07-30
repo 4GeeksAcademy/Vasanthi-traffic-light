fb36bf1d94d290c004b72d4d691e1e5587dfeec2
dande100 <vasanthid10@gmail.com>
2023-07-29 20:03:43 -0500

    Added bonus points

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,28 +1,42 @@
 import React, { useEffect, useState } from 'react'
 import '../../styles/trafficLight.css'
+let trafficLights = ["red", "yellow", "green"]
+let repeatTrafficLights = [...trafficLights]
 
 const TrafficComponent = () => {
   const [choosenColor, setChoosenColor] = useState("")
-  // function setLight() {
-  //   let arr = ['red']
-  //    arr[0].forEach((item, index) => {
-  //     setChoosenColor(item)
-  //   })
-    
-  // }
+  const [showButton, setShowButton] =useState(true)
+  
+  function setLight() {
+    if(repeatTrafficLights.length == 0) {
+      repeatTrafficLights = [...trafficLights]
+    }
+    setChoosenColor(repeatTrafficLights[0])
+    repeatTrafficLights.splice(0, 1)
+  }
+  function extraLightColor() {
+    if (showButton) {
+      trafficLights.push("purple")
+      repeatTrafficLights.push("purple")
+      setChoosenColor('purple')
+      setShowButton(false)
+    }
+  }
+  const listItems = trafficLights.map((item) => {
+    return <div key={item} className={choosenColor == item ? `${item} circle selected`: `${item} circle`} onClick={() => setChoosenColor(item)}></div>
+  })
   return (
     <>
      <div className="traffic-wrapper d-flex justify-content-center align-items-center">
       <div>
       <div className="traffic-handle"></div>
       <div className="traffic-lights">
-        <div className={choosenColor == 'red' ? 'red circle selected' : "red circle"}  onClick={() => setChoosenColor('red')}></div>
-        <div className={choosenColor == 'yellow' ? 'yellow circle selected' : "yellow circle"}  onClick={() => setChoosenColor('yellow')}></div>
-        <div className={choosenColor == 'green' ? 'green circle selected' : "green circle"} onClick={() => setChoosenColor('green')}></div>
+        {listItems}
       </div>
       </div>
      </div>
       <button className='m-auto btn btn-primary' onClick={() => setLight()}>Traffic Light Button</button>
+      <button className='m-auto btn btn-primary' disabled={!showButton} onClick={() => extraLightColor()}>Extra Color</button>
     </>
   )
 }
